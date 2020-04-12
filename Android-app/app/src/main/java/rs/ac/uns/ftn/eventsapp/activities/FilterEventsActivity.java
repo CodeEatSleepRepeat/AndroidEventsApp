11ae55840c1754474a1064d9aa90c614b77c9ef0
MarkoKatic96 <katicmarko96@gmail.com>
2020-04-12 17:21:58 +0200

    Ekran za filtriranje dogadjaja

--- Android-app/app/src/main/java/rs/ac/uns/ftn/eventsapp/activities/FilterEventsActivity.java
+++ Android-app/app/src/main/java/rs/ac/uns/ftn/eventsapp/activities/FilterEventsActivity.java
@@ -2,33 +2,151 @@ package rs.ac.uns.ftn.eventsapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.ColorFilter;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import rs.ac.uns.ftn.eventsapp.R;
 
 public class FilterEventsActivity extends AppCompatActivity {
 
+    final Calendar calendar = Calendar.getInstance();
+    private EditText startingDateEditText;
+    private EditText startingTimeEditText;
+    private EditText endingDateEditText;
+    private EditText endingTimeEditText;
+
     private TextView showProgress;
+    private SeekBar seekbar;
+
     private Button forYouBtn;
     private Button recentBtn;
     private Button popularBtn;
-    private SeekBar seekbar;
 
-    public FilterEventsActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_events);
+
         showProgress = findViewById(R.id.kmFilterTextView);
         seekbar = findViewById(R.id.distanceFilterSeekBar);
+
+        startingDateEditText = findViewById(R.id.startingDateFilterEditText);
+        startingTimeEditText = findViewById(R.id.startingTimeFilterEditText);
+        endingDateEditText = findViewById(R.id.endingDateFilterEditText);
+        endingTimeEditText = findViewById(R.id.endingTimeFilterEditText);
+
+        /*
+        * Listener koji se odnosi na kalendar
+        * Postavlja izabrani pocetni datum u edit text
+        * */
+        final DatePickerDialog.OnDateSetListener startingDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String format = "dd/MM/yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.FRANCE);
+                startingDateEditText.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        /*
+        * Listener koji se odnosi na time picker
+        * Postavlja izabrano pocetno vreme u edit box
+        * */
+        final TimePickerDialog.OnTimeSetListener startingTime = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                if (minute < 10) {
+                    startingTimeEditText.setText(hourOfDay + ":0" + minute);
+                } else {
+                    startingTimeEditText.setText(hourOfDay + ":" + minute);
+                }
+            }
+        };
+
+        startingTimeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(FilterEventsActivity.this, startingTime,
+                        calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        startingDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(FilterEventsActivity.this, startingDate,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        /*
+         * Listener koji se odnosi na kalendar
+         * Postavlja izabrani zavrsni datum u edit text
+         * */
+        final DatePickerDialog.OnDateSetListener endingDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String format = "dd/MM/yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.FRANCE);
+                endingDateEditText.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        /*
+         * Listener koji se odnosi na time picker
+         * Postavlja izabrano zavrsno vreme u edit box
+         * */
+        final TimePickerDialog.OnTimeSetListener endingTime = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                if (minute < 10) {
+                    endingTimeEditText.setText(hourOfDay + ":0" + minute);
+                } else {
+                    endingTimeEditText.setText(hourOfDay + ":" + minute);
+                }
+            }
+        };
+
+        endingTimeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(FilterEventsActivity.this, endingTime,
+                        calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        endingDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(FilterEventsActivity.this, endingDate,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         forYouBtn = findViewById(R.id.forYouFilterBtn);
         recentBtn = findViewById(R.id.recentFilterBtn);
@@ -103,6 +221,4 @@ public class FilterEventsActivity extends AppCompatActivity {
                 break;
         }
     }
-
-
 }
